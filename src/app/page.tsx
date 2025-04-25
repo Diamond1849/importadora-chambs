37c2aac8daeb746a2389ed38420768dc77b46df0
Eduardo9910 <rami.edu.hi@outlook.com>
2025-04-25 13:35:25 -0500

    adde example list detail card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
 import { Icon } from '@iconify/react/dist/iconify.js';
 import DetailCard from '@src/components/DetailCard';
 
+type DetailCardProps = {
+  icono: React.ReactNode;
+  titulo: string;
+  descripcion: string;
+};
+
+const listDetails: DetailCardProps[] = [
+  {
+    icono: <Icon icon={"mdi:abacus"} fontSize={32} />,
+    titulo: "Ttest",
+    descripcion: "test description",
+  },
+  {
+    icono: <Icon icon={"lucide:user"} fontSize={32} />,
+    titulo: "Ttest",
+    descripcion: "test description",
+  },
+  {
+    icono: <Icon icon={"mdi:abacus"} fontSize={32} />,
+    titulo: "Ttest",
+    descripcion: "test description",
+  },
+];
+
 export default function Home() {
-  return <DetailCard titulo="Ttest" descripcion="test description" icono={<Icon icon={"mdi:abacus"} fontSize={32} />} />;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      {listDetails.map((detail, index) => (
+        <DetailCard key={index} {...detail} />
+      ))}
+    </div>
+  );
 }
