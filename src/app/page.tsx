a23383b5c7d48a1cd4a3610c29f5676c14a90d9b
Eduardo9910 <rami.edu.hi@outlook.com>
2025-04-25 13:31:32 -0500

    Added icon example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
+import DetailCard from '@src/components/DetailCard';
+
 export default function Home() {
-  return <div>Hola Mundo</div>;
+  return <DetailCard titulo="Ttest" descripcion="test description" icono={<Icon icon={"mdi:abacus"} fontSize={32} />} />;
 }
